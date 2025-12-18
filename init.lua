8ac88b880c5ca04da1e11a27883fdd77872a3576
Jules <julesbodin04@gmail.com>
2025-12-18 15:30:12 +0100

    fix(plugins): fixed bug caused by version

--- init.lua
+++ init.lua
@@ -24,11 +24,11 @@ require 'core.autocmds'
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -45,7 +45,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 local plugins_specs = require 'plugins'
 local lazy_icons = require 'configs.lazy_ui'
-require('lazy').setup({ plugins_specs }, { ui = { lazy_icons } })
+require('lazy').setup(plugins_specs, { ui = lazy_icons })
 
 -- require 'kickstart.plugins.debug',
 -- require 'kickstart.plugins.indent_line',
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -41,16 +41,25 @@ return {
     {
         'nvim-treesitter/nvim-treesitter-context',
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        lazy = false,
         opts = {
             enable = true,
+            multiwindow = true, -- Enable multiwindow support.
             max_lines = 3, -- Nombre maximum de lignes de contexte à afficher
             min_window_height = 0,
             line_numbers = true,
-            multiline_threshold = 20, -- Maximum number of lines to show for a single context
+            multiline_threshold = 1, -- Maximum number of lines to show for a single context
             trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded
             mode = 'cursor', -- Line used to calculate context. 'cursor' ou 'topline'
-            separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
+            separator = '-', -- Separator between context and content. Should be a single character string, like '-'.
             zindex = 20,
+            node_types = {
+                rust = { 'impl_item', 'function_item', 'struct_item', 'enum_item', 'trait_item' },
+                c = { 'function_definition', 'struct_specifier', 'enum_specifier', 'union_specifier' },
+                python = { 'function_definition', 'class_definition' },
+                lua = { 'function_declaration', 'function_definition' },
+                -- Add other language if needed
+            },
         },
         keys = {
             {
