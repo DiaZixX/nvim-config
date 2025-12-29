40af68cbc0ff2973f49fa7e2e3846bc2373f4f07
Jules <julesbodin04@gmail.com>
2025-12-30 00:43:11 +0100

    fix(lsp): prepare for transition to ruff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,10 +75,14 @@ return {
                     settings = {
                         pylsp = {
                             plugins = {
-                                flake8 = { enabled = true },
-                                pycodestyle = { enabled = false },
-                                mccabe = { enabled = false },
                                 pyflakes = { enabled = false },
+                                pycodestyle = { enabled = false },
+                                autopep8 = { enabled = false },
+                                yapf = { enabled = false },
+                                mccabe = { enabled = false },
+                                pylsp_mypy = { enabled = false },
+                                pylsp_black = { enabled = false },
+                                pylsp_isort = { enabled = false },
                             },
                         },
                     },
